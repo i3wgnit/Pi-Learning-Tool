81ff92d197992c12882a0d123df9fdfd88fd4578
i3wgnit <i3wgnit@users.noreply.github.com>
2017-10-14 18:40:27 -0400

    Update pi-learning.user.js

--- pi-learning.user.js
+++ pi-learning.user.js
@@ -12,7 +12,7 @@
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
 // @downloadURL https://rawgit.com/i3wgnit/Pi-Learning-Tool/master/pi-learning.user.js
-// @version     2.4.3+
+// @version     2.4.4
 // ==/UserScript==
 var GAME = {};
 
