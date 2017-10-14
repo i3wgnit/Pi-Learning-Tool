df2a254bde9aca7105078474585dbc9476a31a8d
i3wgnit <i3wgnit@users.noreply.github.com>
2017-10-14 18:40:18 -0400

    Update pi-learning.user.js

--- pi-learning.user.js
+++ pi-learning.user.js
@@ -8,7 +8,6 @@
 // @include     https://*youtube.com/*
 // @include     http://*reddit.com/*
 // @include     https://*reddit.com/*
-// @run-at      document-end
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
